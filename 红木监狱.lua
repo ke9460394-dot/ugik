fe3ed66288ad9e6ffdee23aed5667cfc25ac6cb2
ke9460394-dot <ke9460394@gmail.com>
2025-10-03 23:13:58 +0800

    k

---
+++ 红木监狱.lua
@@ -0,0 +1,243 @@
+-----不加密了二改可以别删这个注释我的b站UID：1531514159
+local V = {
+    ["Fists Tool Sounds"] = "拳击工具声音",
+    ["Chainsaw Active"] = "电锯激活",
+    ["Chainsaw Idle"] = "电锯空转",
+    ["Chainsaw Ended"] = "电锯结束",
+    ["Punch Hit"] = "拳击命中",
+    ["Gun Stuff"] = "枪械物品",
+    ["Refill Ammo"] = "补充弹药",
+    ["For Axe"] = "斧头",
+    ["Kill"] = "击杀",
+    ["Fling"] = "抛飞",
+    ["Vaporize"] = "汽化",
+    ["TP To"] = "传送至",
+    ["Bring Car To"] = "带车到",
+    ["Spam Vaporize Target"] = "重复汽化目标",
+    ["View Mods"] = "视图模组",
+    ["Target Name"] = "目标名称",
+    ["Noclip All Doors"] = "无碰撞所有门",
+    ["AutoRespawn"] = "自动重生",
+    ["Anti Arrest"] = "反逮捕",
+    ["Select Gun"] = "选择枪械",
+    ["Autospawn With Gun"] = "携带枪械自动生成",
+    ["Access Restricted Areas"] = "访问受限区域",
+    ["The Prison"] = "监狱",
+    ["Cop Lounge"] = "警察休息室",
+    ["Heli Pad"] = "直升机停机坪",
+    ["Tower Roof"] = "塔顶",
+    ["Front of Gates"] = "大门前",
+    ["Main Armory"] = "主军械库",
+    ["Melee"] = "近战",
+    ["Hammer"] = "锤子",
+    ["Bat"] = "棒球棍",
+    ["Knife"] = "刀",
+    ["Sledgehammer"] = "大锤",
+    ["Fireaxe"] = "消防斧",
+    ["Game Settings"] = "游戏设置",
+    ["UMP-45"] = "UMP-45",
+    ["Sten"] = "斯登",
+    ["M14"] = "M14",
+    ["PUMPKIN-7"] = "南瓜-7",
+    ["Model 1866"] = "1866型",
+    ["Double Barrel"] = "双管",
+    ["C4"] = "C4",
+    ["Rusted Keycard"] = "生锈的钥匙卡",
+    ["Binoculars"] = "双筒望远镜",
+    ["Molotov"] = "莫洛托夫鸡尾酒",
+    ["Fireworks"] = "烟花",
+    ["Grenade"] = "手榴弹",
+    ["Garage Roof"] = "车库屋顶",
+    ["Garage"] = "车库",
+    ["Front of Prison"] = "监狱前",
+    ["Cafeteria"] = "食堂",
+    ["J-Block"] = "J区",
+    ["Secret Lounge"] = "秘密休息室",
+    ["Katana"] = "武士刀",
+    ["Kitchen Knife"] = "厨房刀",
+    ["Tomahawk"] = "战斧",
+    ["Misc"] = "杂项",
+    ["Bandage"] = "绷带",
+    ["Smoke-Grenade"] = "烟雾弹",
+    ["FZ2"] = "FZ2",
+    ["Secondary Armory"] = "次级军械库",
+    ["Prison Roof"] = "监狱屋顶",
+    ["The City"] = "城市",
+    ["Car Dealer"] = "汽车经销商",
+    ["Fugitive Base"] = "逃犯基地",
+    ["Car Shop"] = "汽车商店",
+    ["Fugitive Base Roof"] = "逃犯基地屋顶",
+    ["Tallest Structure"] = "最高建筑",
+    ["Hotel"] = "酒店",
+    ["Gas Station"] = "加油站",
+    ["The Outside"] = "外部",
+    ["House"] = "房屋",
+    ["Docks"] = "码头",
+    ["Light House"] = "灯塔",
+    ["Campfire"] = "营火",
+    ["Waterfall"] = "瀑布",
+    ["Mountains"] = "山脉",
+    ["The Darkones Redwood Prison - Reworked GUI"] = "黑暗之子红木监狱 - 重制界面",
+    ["Abilities"] = "能力",
+    ["Vehicles"] = "载具",
+    ["Free Weapons"] = "免费武器",
+    ["Team"] = "团队",
+    ["Player"] = "玩家",
+    ["M24"] = "M24",
+    ["M93"] = "M93",
+    ["Fugitive Guns"] = "逃犯枪械",
+    ["AS VAL Guns"] = "AS VAL 枪械",
+    ["MB3"] = "MB3",
+    ["MBAS"] = "MBAS",
+    ["The Darkones Empire Crowbar"] = "黑暗之子帝国撬棍",
+    ["Click Kill"] = "点击击杀",
+    ["Click TP"] = "点击传送",
+    ["Click Fling"] = "点击抛飞",
+    ["Click Vaporize"] = "点击汽化",
+    ["Click Explode Vehicles"] = "点击爆炸载具",
+    ["Prisoners"] = "囚犯",
+    ["Guards"] = "守卫",
+    ["Fugitive"] = "逃犯",
+    ["Sedan"] = "轿车",
+    ["SUV"] = "SUV",
+    ["Armored Truck"] = "装甲卡车",
+    ["Ambulance"] = "救护车",
+    ["Interceptor"] = "拦截车",
+    ["WalkSpeed"] = "行走速度",
+    ["Fly"] = "飞行",
+    ["Walk Speed"] = "行走速度",
+    ["Fly Speed"] = "飞行速度",
+    ["Max View Distance"] = "最大视野距离",
+    ["OverPowered"] = "超能力",
+    ["Kill All Enemies"] = "击杀所有敌人",
+    ["Arrest All Enemies"] = "逮捕所有敌人",
+    ["Vaporize All Enemies"] = "汽化所有敌人",
+    ["Fling All Enemies"] = "抛飞所有敌人",
+    ["SHOCKWAVE-870"] = "冲击波-870",
+    ["Guards Guns"] = "守卫枪械",
+    ["AS-VAL"] = "AS-VAL",
+    ["Express"] = "快递车",
+    ["Pickup Truck"] = "皮卡车",
+    ["TRX"] = "TRX",
+    ["Helicopter"] = "直升机",
+    ["Guns"] = "枪械",
+    ["Thumper"] = "雷管",
+    ["AWP"] = "AWP",
+    ["FN-17"] = "FN-17",
+    ["MK48"] = "MK48",
+    ["Snubnose"] = "短管左轮",
+    ["SMG"] = "冲锋枪",
+    ["PSSH-41"] = "PPSh-41",
+    ["Glock 17"] = "格洛克17",
+    ["TAC-50"] = "TAC-50",
+    ["Serbu Shorty"] = "塞尔布短管",
+    ["Move Controls"] = "移动控制",
+    ["Camera"] = "相机",
+    ["The Darkones Redwood Prison-Reworked GUI"] = "Kenny汉化b站UID:1531514159",
+["Buyable Weapons"] = "可购买武器",
+["Teleports"] = "传送点",
+["Target Player"] = "目标玩家",
+["Extra"] = "额外",
+["The Darkones Empire"] = "黑暗帝国",
+["View"] = "查看",
+["Refill Ammo"] = "补充弹药",
+["Buyable Weapons"] = "可购买武器",
+["Teleports"] = "传送点",
+["Front of Gates"] = "大门前",
+["Target Player"] = "目标玩家",
+["Extra"] = "额外",
+["The Darkones Empire"] = "黑暗帝国",
+["Front of Prison"] = "监狱前",
+["Vaporize"] = "汽化",
+["Spam Vaporize Target"] = "连续汽化目标",
+["View"] = "查看",
+["Cafeteria"] = "自助餐厅",
+["Cell Block A"] = "A区牢房",
+["Cell Block B"] = "B区牢房",
+["Infirmary"] = "医务室",
+["Guard Quarters"] = "守卫宿舍",
+["Click Vaporize"] = "点击汽化",
+["Lake"] = "湖泊",
+["Yard"] = "操场",
+["Waterfall"] = "瀑布",
+["Weapon Store"] = "武器商店",
+["Gas Station"] = "加油站",
+["Big Spoon"] = "大勺",
+["Machete"] = "砍刀",
+["Kitchen Knife"] = "厨房刀",
+["Armory A"] = "A武器库",
+["Armory B"] = "B武器库",
+["Refill Ammo"] = "补充弹药",
+["OP"] = "超强",
+["Vaporize All Enemies"] = "汽化所有敌人",
+}
+
+local function isTextInstance(inst)
+    return inst:IsA("TextLabel") or inst:IsA("TextButton") or inst:IsA("TextBox")
+end
+
+local connected = setmetatable({}, { __mode = "k" })
+
+local function translateInstanceText(inst)
+    if not isTextInstance(inst) then return end
+    if connected[inst] then return end
+    connected[inst] = true
+
+    local curr = inst.Text
+    if type(curr) == "string" and V[curr] then
+        inst:SetAttribute("_translating", true)
+        inst.Text = V[curr]
+        inst:SetAttribute("_translating", nil)
+    end
+
+    inst:GetPropertyChangedSignal("Text"):Connect(function()
+        if inst:GetAttribute("_translating") then return end
+        local newText = inst.Text
+        if type(newText) == "string" and V[newText] then
+            inst:SetAttribute("_translating", true)
+            inst.Text = V[newText]
+            inst:SetAttribute("_translating", nil)
+        end
+    end)
+end
+
+local function initForParent(parent)
+    for _, desc in ipairs(parent:GetDescendants()) do
+        if isTextInstance(desc) then
+            translateInstanceText(desc)
+        end
+    end
+end
+
+local Players = game:GetService("Players")
+local localPlayer = Players.LocalPlayer
+local CoreGui = game:GetService("CoreGui")
+
+initForParent(CoreGui)
+CoreGui.DescendantAdded:Connect(function(desc)
+    if isTextInstance(desc) then
+        translateInstanceText(desc)
+    end
+end)
+
+if localPlayer then
+    local function hookPlayerGui(pg)
+        if not pg then return end
+        initForParent(pg)
+        pg.DescendantAdded:Connect(function(desc)
+            if isTextInstance(desc) then
+                translateInstanceText(desc)
+            end
+        end)
+    end
+    if localPlayer:FindFirstChild("PlayerGui") then
+        hookPlayerGui(localPlayer.PlayerGui)
+    end
+    localPlayer.ChildAdded:Connect(function(child)
+        if child.Name == "PlayerGui" then
+            hookPlayerGui(child)
+        end
+    end)
+end
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/TheDarkoneMarcillisePex/Other-Scripts/refs/heads/main/Redwood%20Prison%20Reworked'))()
